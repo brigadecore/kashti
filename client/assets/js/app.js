91f0789d4ddb20cbc974cc877c100bd6a73367c3
flynnduism <flynnduism@gmail.com>
2017-08-29 16:17:19 -0700

    connecting acid-api to angular controllers

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -31,4 +31,19 @@
     FastClick.attach(document.body);
   }
 
+  // consume api for templates/views
+  app.controller("projectController", function ($scope, $http) {
+    $http({method: 'GET',
+      url: 'http://localhost:7745/v1/project/acid-830c16d4aaf6f5490937ad719afd8490a5bcbef064d397411043ac/',
+      headers: {
+        // 'Access-Control-Allow-Origin': 'http://localhost'
+      }
+    })
+
+    .success(function (response){
+      $scope.projects = response.data;
+    })
+
+  });
+
 })();
