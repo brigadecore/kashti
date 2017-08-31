cc424e16264601c9a01b8582723a6b9688103e77
flynnduism <flynnduism@gmail.com>
2017-08-31 15:02:58 -0700

    wiring up project and build api endpoints

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -32,18 +32,31 @@
   }
 
   // consume api for templates/views
+
+  app.controller("buildController", function ($scope, $http) {
+    $http({method: 'GET',
+      url: 'http://localhost:7745/v1/build/:test-1234567890abcdef-12345678/',
+      headers: {
+        // 'Access-Control-Allow-Origin': 'http://localhost:7745/'
+      }
+    }).then(function successCallback(response) {
+        $scope.build = response.data;
+    },
+      function errorCallback(response) {}
+    );
+  });
+
   app.controller("projectController", function ($scope, $http) {
     $http({method: 'GET',
       url: 'http://localhost:7745/v1/project/acid-830c16d4aaf6f5490937ad719afd8490a5bcbef064d397411043ac/',
       headers: {
-        // 'Access-Control-Allow-Origin': 'http://localhost'
+        // 'Access-Control-Allow-Origin': 'http://localhost:7745/'
       }
-    })
-
-    .success(function (response){
-      $scope.projects = response.data;
-    })
-
+    }).then(function successCallback(response) {
+        $scope.project = response.data;
+    },
+      function errorCallback(response) {}
+    );
   });
 
 })();
