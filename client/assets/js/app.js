ba9160c4e1c1cd7e586d74878b564b6a5db92504
flynnduism <flynnduism@gmail.com>
2017-09-07 22:31:45 -0700

    configuring routes to use {{project.id}} and {{build.id}} paths

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -36,6 +36,15 @@
     FastClick.attach(document.body);
   }
 
+  app.filter('trim', function () {
+    return function(value) {
+      if(!angular.isString(value)) {
+          return value;
+      } 
+      return value.replace(/^\s+|\s+$/g, ''); // you could use .trim, but it's not going to work in IE<9
+    };
+  });
+
   // consume api for templates/views
   app.controller("projectsController", function ($scope, $http) {
     $http({
