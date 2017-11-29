9a4afdef570d3a5ba7b408a4824cf9cf4eb24922
flynnduism <flynnduism@gmail.com>
2017-11-29 14:22:46 -0800

    Change <title> tag to kashti

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -29,6 +29,12 @@
 
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
 
+  app.filter('capitalize', function() {
+      return function(input) {
+        return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+      }
+  });
+
   function config($urlProvider, $locationProvider) {
     $urlProvider.otherwise('/');
 
