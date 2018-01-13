ba2376608512b5c56a0a7f154e97db0d64d0950d
Sean Knox <knoxville@gmail.com>
2018-01-12 18:11:07 -0800

    fix(tests): ensure sidebar component available in app component specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        SidebarComponent
       ],
       imports: [
         RouterTestingModule.withRoutes(
