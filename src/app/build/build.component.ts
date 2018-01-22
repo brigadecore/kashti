bb02d7deed470a2224be3a6ad19a98fe07c1a5a8
Ronan Flynn-Curran <flynnduism@gmail.com>
2018-01-22 09:59:43 -0800

    add build component

---
+++ src/app/build/build.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-build',
+  templateUrl: './build.component.html',
+  styleUrls: ['./build.component.scss']
+})
+export class BuildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
