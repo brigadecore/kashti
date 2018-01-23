7e6fd95be1b2ed5eb99e9674195bd74339a0db78
Ronan Flynn-Curran <flynnduism@gmail.com>
2018-01-23 14:38:24 -0800

    adding build page static content, hook up routing for /build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { BuildComponent } from './build/build.component';
 import { StyleGuideComponent } from './style-guide/style-guide.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'styleguide', component: StyleGuideComponent },
+  { path: 'build', component: BuildComponent },
+  { path: 'styleguide', component: StyleGuideComponent }
 ];
 
 @NgModule({
