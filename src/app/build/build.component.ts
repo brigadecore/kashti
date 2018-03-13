cb9061647ed0ec7eb12f986c07c175578fb8a729
Ronan Flynn-Curran <flynnduism@gmail.com>
2018-03-13 13:56:20 +0000

    update BuildComponent to consume BuildService

--- src/app/build/build.component.ts
+++ src/app/build/build.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+
+import { MomentModule } from 'angular2-moment';
+
+import { Build, Project } from '../models/project';
+import { BuildService } from '../build.service';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'app-build',
   templateUrl: './build.component.html',
   styleUrls: ['./build.component.scss']
 })
-export class BuildComponent implements OnInit {
 
-  constructor() { }
+export class BuildComponent implements OnInit {
+  build;
+
+  constructor(
+    private buildService: buildService,
+    private route: ActivatedRoute,
+
+  ) { }
 
   ngOnInit() {
+    const projectId = this.route.snapshot.paramMap.get('id');
+    this.getBuild(buildId);
   }
 
+  getBuild(buildId): void {
+    this.buildService.getBuild(projectId)
+      .subscribe(returnedBuild => {
+        this.build = returnedBuild;
+      },
+      error => console.error(error));
+  }
 }
