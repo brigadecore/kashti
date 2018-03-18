17420f5754b2e51b38e0d228044cc859a5d07afc
Sean Knox <knoxville@gmail.com>
2018-03-17 23:16:04 -0700

    fix(tests): fix project component spec

--- src/app/services/project.resolver.ts
+++ src/app/services/project.resolver.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
 
 import { ProjectService } from './project/project.service';
 import { Project } from '../models/Project';
@@ -9,7 +11,8 @@ export class ProjectResolver implements Resolve<Project> {
 
   constructor(private projectService: ProjectService) {}
 
-  resolve(route: ActivatedRouteSnapshot) {
-    return this.projectService.getProject(route.paramMap.get('id'));
+  resolve(route: ActivatedRouteSnapshot): Observable<Project> {
+    return this.projectService.getProject(route.paramMap.get('id'))
+      .catch((e: any) => Observable.throw(console.log(e)));
   }
 }
