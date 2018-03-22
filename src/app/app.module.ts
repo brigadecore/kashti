4867dd42905276a3fb0a6d6dce30cf801a0974aa
Ronan Flynn-Curran <flynnduism@gmail.com>
2018-03-22 15:30:26 +0000

    add baseline log component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,6 +30,8 @@ import { BuildResolver } from './services/resolvers/build.resolver';
 import { JobResolver } from './services/resolvers/job.resolver';
 import { LogResolver } from './services/resolvers/log.resolver';
 import { SortBuildByStartDatePipe } from './pipes/builds/sort-build-by-start-date.pipe';
+import { JobComponent } from './job/job.component';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
   declarations: [
