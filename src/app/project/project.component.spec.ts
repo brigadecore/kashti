9dfa6928b1ce78f3ac85893a3c02f0e07a1ddf0f
Sean Knox <knoxville@gmail.com>
2018-03-21 17:44:11 -0700

    chore(project spec): add test for CalculateProviderLogoClass

--- src/app/project/project.component.spec.ts
+++ src/app/project/project.component.spec.ts
@@ -117,13 +117,13 @@ describe('ProjectComponent', () => {
       expect(component).toBeTruthy();
     });
 
-    it('should set the project', () => {
+    it('should set the project', async(() => {
       expect(component.project).toEqual(project);
-    });
+    }));
 
-    it('should set the builds', () => {
+    it('should set the builds', async(() => {
       expect(component.builds).toEqual(builds);
-    });
+    }));
   });
 
   describe('when project and build data is unavailable', () => {
@@ -142,5 +142,29 @@ describe('ProjectComponent', () => {
     });
   });
 
+  describe('CalculateProviderLogoClass', () => {
+    let expected;
 
+    describe('when github is the argument', () => {
+      beforeEach(() => {
+        expected = 'icon ion-logo-github';
+      });
+
+      it('returns a github icon', () => {
+        expect(component.CalculateProviderLogoClass('github')).toEqual(expected);
+      });
+    });
+
+    describe('for any other argument', () => {
+      beforeEach(() => {
+        expected = 'icon ion-logo-tux';
+      });
+
+      it('returns a tux icon', () => {
+        expect(component.CalculateProviderLogoClass('foo')).toEqual(expected);
+        expect(component.CalculateProviderLogoClass('bar')).toEqual(expected);
+        expect(component.CalculateProviderLogoClass('baz')).toEqual(expected);
+      });
+    });
+  });
 });
