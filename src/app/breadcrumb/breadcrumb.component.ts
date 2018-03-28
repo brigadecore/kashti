3411ec8012135db1cccf1cb3d93d68c3f01c5b44
Ronan Flynn-Curran <flynnduism@gmail.com>
2018-03-28 14:57:31 +0100

    [#165] (WIP) update component with ActivatedRoute

--- src/app/breadcrumb/breadcrumb.component.ts
+++ src/app/breadcrumb/breadcrumb.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
 
 import { Project } from '../models/Project';
 import { Build } from '../models/Build';
+
 
 @Component({
   selector: 'app-breadcrumb',
@@ -9,10 +13,14 @@ import { Build } from '../models/Build';
   styleUrls: ['./breadcrumb.component.scss']
 })
 export class BreadcrumbComponent implements OnInit {
+  project: Project;
+  build: Build;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.project = this.route.snapshot.data['project'];
+    this.build = this.route.snapshot.data['build'];
   }
 
 }
