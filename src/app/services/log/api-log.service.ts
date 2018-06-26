9e680396a52ea6889812b9b86bc0238e7173d0c3
flynnduism <flynnduism@gmail.com>
2018-06-26 16:39:11 -0700

    [118] testing logs api - temp edit

--- src/app/services/log/api-log.service.ts
+++ src/app/services/log/api-log.service.ts
@@ -24,9 +24,10 @@ export class ApiLogService implements LogService {
                                       // test IDs for debugging jobID
                                       // large log: az-01ce52bg33gyjm8nj1x5hzcb51
                                       // small log:  end-run-01ce4rm99edd09dk82zh9h5pxt
-    const logUrl = `${BRIGADE_API_HOST}/v1/job/${jobId}/logs`;
+    const logUrl = `${BRIGADE_API_HOST}/v1/job/helmdocs-01cgw3wkwz9r9t62sy5dvr3494/logs`;
     const options = {responseType: 'text' as 'json'};
 
-    return this.http.get<Log>(logUrl, options);
+    return this.http
+      .get<Log>(logUrl, options);
   }
 }
