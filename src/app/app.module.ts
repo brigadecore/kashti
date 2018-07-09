39f8dee9c974a237e5b5601d9fffbf73ec9889a4
flynnduism <flynnduism@gmail.com>
2018-07-09 16:20:47 -0700

    [118] update app.module following rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,10 +30,6 @@ import { BuildResolver } from './services/resolvers/build.resolver';
 import { JobResolver } from './services/resolvers/job.resolver';
 import { LogResolver } from './services/resolvers/log.resolver';
 import { SortBuildByStartDatePipe } from './pipes/builds/sort-build-by-start-date.pipe';
-import { JobComponent } from './job/job.component';
-import { LogComponent } from './log/log.component';
-import { JobResolver } from './services/resolvers/job.resolver';
-import { LogResolver } from './services/resolvers/log.resolver';
 
 @NgModule({
   declarations: [
